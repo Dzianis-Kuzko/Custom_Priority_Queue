ac6f9c8b0a6f2ade9ce1c473ef052690b1b42fda
Dzianis Kuzko <dkuzko@tut.by>
2024-09-27 19:28:55 +0300

    исправление замечаний

--- src/main/java/ru/clevertec/priority_queue/PriorityQueue.java
+++ src/main/java/ru/clevertec/priority_queue/PriorityQueue.java
@@ -9,9 +9,7 @@ public class PriorityQueue<E> implements Queue<E> {
     private static final int DEFAULT_INITIAL_CAPACITY = 8;
 
     private final Comparator<? super E> comparator;
-
-    private Object[] elements;
-
+    private E[] elements;
     private int size;
 
     public PriorityQueue() {
@@ -19,11 +17,10 @@ public class PriorityQueue<E> implements Queue<E> {
     }
 
     public PriorityQueue(Comparator<? super E> comparator) {
-        this.elements = new Object[DEFAULT_INITIAL_CAPACITY];
+        this.elements = (E[]) new Object[DEFAULT_INITIAL_CAPACITY];
         this.comparator = comparator;
         this.size = 0;
     }
-
 
     @Override
     public boolean add(E e) {
@@ -32,7 +29,7 @@ public class PriorityQueue<E> implements Queue<E> {
         }
 
         if (comparator == null && !(e instanceof Comparable)) {
-            throw new ClassCastException();
+            throw new ClassCastException("Elements must be comparable or a comparator must be provided");
         }
 
         if (elements.length == size) {
@@ -47,25 +44,22 @@ public class PriorityQueue<E> implements Queue<E> {
 
     @Override
     public E peek() {
-        return (E) elements[0];
+        return elements[0];
     }
 
     @Override
     public E poll() {
-        E result = (E) elements[0];
-
-        if (size > 0) {
-            elements[0] = elements[size - 1];
-            elements[size - 1] = null;
-            size--;
-
-            if (comparator == null) {
-                siftDownComparable();
-            } else {
-                siftDownUsingComparator();
-            }
-
+        if (size == 0) {
+            return null;
         }
+
+        E result = elements[0];
+        elements[0] = elements[size - 1];
+        elements[size - 1] = null;
+        size--;
+
+        siftDown();
+
         return result;
     }
 
@@ -74,17 +68,9 @@ public class PriorityQueue<E> implements Queue<E> {
 
         while (k > 0) {
             int parent = (k - 1) / 2;
-            E p = (E) elements[parent];
+            E p = elements[parent];
 
-            int comparisonResult;
-
-            if (comparator == null) {
-                comparisonResult = ((Comparable<? super E>) e).compareTo(p);
-            } else {
-                comparisonResult = comparator.compare(e, p);
-            }
-
-            if (comparisonResult >= 0) {
+            if (compare(e, p) >= 0) {
                 break;
             }
 
@@ -94,54 +80,35 @@ public class PriorityQueue<E> implements Queue<E> {
         elements[k] = e;
     }
 
-    public void siftDownComparable() {
-        int parent = 0;
+    private void siftDown() {
+        int parent =0;
         int halfSize = size / 2;
 
         while (parent < halfSize) {
             int left = parent * 2 + 1;
             int right = left + 1;
+            int smallest = left;
 
-            Comparable<? super E> p = (Comparable<? super E>) elements[parent];
-
-            if (right < size && ((Comparable<? super E>) elements[left]).compareTo((E) elements[right]) > 0) {
-                if (p.compareTo((E) elements[right]) <= 0) {
-                    break;
-                }
-                swap(parent, right, elements);
-                parent = right;
-
-            } else {
-                swap(parent, left, elements);
-                parent = left;
+            if (right < size && compare(elements[left], elements[right]) > 0) {
+                smallest = right;
             }
+
+            if (compare(elements[parent], elements[smallest]) <= 0) {
+                break;
+            }
+
+            swap(parent, smallest, elements);
+            parent = smallest;
         }
     }
 
-    public void siftDownUsingComparator() {
-        int parent = 0;
-        int halfSize = size / 2;
-
-        while (parent < halfSize) {
-            int left = parent * 2 + 1;
-            int right = left + 1;
-
-            E p = (E) elements[parent];
-
-            if (right < size && (comparator.compare((E) elements[left], (E) elements[right]) > 0)) {
-                if (comparator.compare(p, (E) elements[right]) <= 0) {
-                    break;
-                }
-                swap(parent, right, elements);
-                parent = right;
-
-            } else {
-                swap(parent, left, elements);
-                parent = left;
-            }
+    private int compare(E e1, E e2) {
+        if (comparator == null) {
+            return ((Comparable<? super E>) e1).compareTo(e2);
+        } else {
+            return comparator.compare(e1, e2);
         }
     }
-
 
     private void grow() {
         elements = Arrays.copyOf(elements, size + size / 2);
